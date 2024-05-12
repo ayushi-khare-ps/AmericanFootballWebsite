aa7ce621d608dd2f287e074db2de3643bcde5e44
ayushi-khare <ayushi-khare@pluralsight.com>
2024-05-12 18:18:55 +0530

    selection message display

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,60 @@
 "use strict";
 
-window.onload = function (){
+window.onload = function () {
     initAmericanFootballTeams();
+
+    //const AFT = document.getElementById("favTeam");
+
 }
 
 let teams = [
-    { teamcode:"DAL", 
-      name:"Dallas Cowboys", 
-      locatedIn:"Arlington, TX"
+    {
+        teamcode: "DAL",
+        name: "Dallas Cowboys",
+        locatedIn: "Arlington, TX"
     },
-    { teamcode:"DEN", 
-      name:"Denver Broncos", 
-      locatedIn:"Denver, CO"
+    {
+        teamcode: "DEN",
+        name: "Denver Broncos",
+        locatedIn: "Denver, CO"
     },
-    { teamcode:"HOU", 
-      name:"Houston Texans", 
-      locatedIn:"Houston, TX"
+    {
+        teamcode: "HOU",
+        name: "Houston Texans",
+        locatedIn: "Houston, TX"
     },
-    { teamcode:"KAN", 
-      name:"Kansas City Chiefs", 
-      locatedIn:"Kansas City, MO"
+    {
+        teamcode: "KAN",
+        name: "Kansas City Chiefs",
+        locatedIn: "Kansas City, MO"
     }
-  ];
+];
 
-  function initAmericanFootballTeams(){
+function initAmericanFootballTeams() {
     let teamList = document.getElementById("favTeam");
     let teamLength = teams.length;
 
-    for (let i=0; i< teamLength; i++){
+    for (let i = 0; i < teamLength; i++) {
         let theOption = new Option(teams[i].name, teams[i].teamcode);
         teamList.appendChild(theOption);
     }
+}
 
+//const selBtn = document.getElementById("selectBtn");
+
+function displayMsg() {
+
+    const favTeam = document.getElementById("favTeam");
+    let favTeamIndex = favTeam.selectedIndex;
+    const msgField = document.getElementById("your-selection");
+    if (favTeamIndex != 0){
     
-  }
+
+        msgField.innerHTML = `You selected the ${teams[favTeamIndex - 1].name} who plays in ${teams[favTeamIndex - 1 ].locatedIn}`;
+    }
+    else{
+
+        msgField.innerHTML = "";
+    }
+
+}
